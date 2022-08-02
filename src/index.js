f28084a1ed75507a99dbebb068dab275aae3106d
Ghislain Mitahi <ghislainmitahi@gmail.com>
2022-08-02 09:48:00 +0200

    fixing linter issues

--- src/index.js
+++ src/index.js
@@ -1,49 +1,39 @@
 const title = document.getElementById('title');
 const author = document.getElementById('author');
-
 const addBtn = document.getElementById('add');
 const list = document.getElementById('list');
 
-
-
-{/* <div class="bookCard">
-<p class="bookTitle">Lorem impsum</p>
-<p class="bookAuthor">
-  Testeroo testyy
-</p>
-<button class="remove">Remove</button>
-<hr>
-<br>
-</div> */}
-
-class book {
-  constructor(title, author) {
-    this.title = title, 
-    this.author = author
+class Book {
+  Constructor(title, author) {
+    this.title = title;
+    this.author = author;
   }
 }
 
 addBtn.addEventListener('click', (e) => {
   e.preventDefault();
- let titleText = title.value;
- let authorText = author.value; 
- const newBook = new book(titleText, authorText);
-if(localStorage.getItem('books')) {
-  let savedBooks = JSON.parse( localStorage.getItem('books'));
-  savedBooks = [ newBook, ...savedBooks]
-  localStorage.setItem('books', JSON.stringify(savedBooks));
-
-}else{
-
-  localStorage.setItem('books', JSON.stringify([newBook]));
-}
-document.location.reload();
+  const titleText = title.value;
+  const authorText = author.value;
+  const newBook = new Book(titleText, authorText);
+  if (localStorage.getItem('books')) {
+    let savedBooks = JSON.parse(localStorage.getItem('books'));
+    savedBooks = [newBook, ...savedBooks];
+    localStorage.setItem('books', JSON.stringify(savedBooks));
+  } else {
+    localStorage.setItem('books', JSON.stringify([newBook]));
+  }
+  document.location.reload();
 });
 
+const listbooks = JSON.parse(localStorage.getItem('books'));
 
+const removeTheBook = (id) => {
+  const list = listbooks.filter((book) => book.title !== listbooks[id].title);
+  localStorage.setItem('books', JSON.stringify(list));
+  document.location.reload();
+};
 
-var listbooks = JSON.parse(localStorage.getItem('books'));
-listbooks.forEach(book => {
+listbooks.forEach((book) => {
   const bookCard = document.createElement('div');
   bookCard.classList.add('bookCard');
   const bookTitle = document.createElement('p');
@@ -54,12 +44,14 @@ listbooks.forEach(book => {
   bookAuthor.innerText = book.author;
   const removeBtn = document.createElement('button');
   removeBtn.classList.add('remove');
-  removeBtn.innerText = 'Remove'
+  removeBtn.innerText = 'Remove';
+  const id = listbooks.indexOf(book);
+  removeBtn.addEventListener('click', () => {
+    removeTheBook(id);
+  });
   const hr = document.createElement('hr');
   const br = document.createElement('br');
 
   bookCard.append(bookTitle, bookAuthor, removeBtn, hr, br);
   list.append(bookCard);
 });
-
-
