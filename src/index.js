d427e5059c05780427aa3c640dd87b3478b1193c
Ghislain Mitahi <ghislainmitahi@gmail.com>
2022-08-02 16:48:40 +0200

    add font in styling

--- src/index.js
+++ src/index.js
@@ -3,26 +3,24 @@ const author = document.getElementById('author');
 const addBtn = document.getElementById('add');
 const list = document.getElementById('list');
 
-
-
 class Book {
   constructor(title, author) {
     this.title = title;
     this.author = author;
   }
 
-  add (title, author) {
-    const newBook = {title, author}
+  this.add(title, author) {
+    const newBook = { title, author };
     if (localStorage.getItem('books')) {
-    let savedBooks = JSON.parse(localStorage.getItem('books'));
-    savedBooks = [newBook, ...savedBooks];
-    localStorage.setItem('books', JSON.stringify(savedBooks));
-  } else {
-    localStorage.setItem('books', JSON.stringify([newBook]));
-  }
+      let savedBooks = JSON.parse(localStorage.getItem('books'));
+      savedBooks = [newBook, ...savedBooks];
+      localStorage.setItem('books', JSON.stringify(savedBooks));
+    } else {
+      localStorage.setItem('books', JSON.stringify([newBook]));
+    }
   }
 
-  remove (id) {
+  remove(id) {
     const list = listbooks.filter((book) => book.title !== listbooks[id].title);
     localStorage.setItem('books', JSON.stringify(list));
   }
@@ -31,11 +29,11 @@ class Book {
 const newBook = new Book();
 
 addBtn.addEventListener('click', (e) => {
-    e.preventDefault();
-    const titleText = title.value;
-    const authorText = author.value;
-    newBook.add(titleText, authorText);
-    document.location.reload();
+  e.preventDefault();
+  const titleText = title.value;
+  const authorText = author.value;
+  newBook.add(titleText, authorText);
+  document.location.reload();
 });
 
 const listbooks = JSON.parse(localStorage.getItem('books'));
@@ -53,7 +51,7 @@ listbooks.forEach((book) => {
   removeBtn.classList.add('remove');
   removeBtn.innerText = 'Remove';
   const id = listbooks.indexOf(book);
-  if(id % 2 === 0){
+  if (id % 2 === 0) {
     bookCard.style.backgroundColor = '#999';
   }
   removeBtn.addEventListener('click', () => {
